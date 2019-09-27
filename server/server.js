ace62b1ccdd1ed22ed44b9473027508bf2ac3ab1
Sung Soo (Anthony) Cho <sc80@rice.edu>
2019-09-27 15:34:32 -0500

    Added cors

--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require('cors');
 const app = express()
 const port = 3000
+
+app.use(cors());
 
 app.use(bodyParser.json())
 app.use(
