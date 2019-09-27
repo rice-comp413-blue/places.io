b1cd768944c84056851ff9f64db65564888c7c50
klin1344 <kevlin97@gmail.com>
2019-09-27 17:44:17 -0500

    Client: display map view

---
+++ client/src/MapView/MapView.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../App.css';
+import { Map, TileLayer } from 'react-leaflet';
+const MapView = () => (
+    <Map center={[29.749907, -95.358421]} zoom={10}
+        style={{ height: '100vh', width: '100vw' }}>
+        <TileLayer
+            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+    </Map>
+);
+
+export default MapView;
