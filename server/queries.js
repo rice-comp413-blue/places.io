c70cb8017305a976689cd3d0d6fa76569a1a323c
samrabelachew <sdb13@rice.edu>
2019-09-29 11:07:47 -0500

    dockerized a postgres container and composed two containers together

--- server/queries.js
+++ server/queries.js
@@ -1,15 +1,26 @@
 const Pool = require('pg').Pool
 const pool = new Pool({
   user: 'blueteam',
-  host: 'localhost',
+  host: process.env['HOST'],
   database: 'app',
   password: 'password',
   port: 5432,
 })
 
+const psqlTest = (request, response) => {
+  pool.query('SELECT 5 * 5', (error, results) => {
+    if (error) {
+      response.status(404).json(error.toString())
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
+
 const getTest = (request, response) => {
   pool.query('SELECT * FROM test', (error, results) => {
     if (error) {
+      response.status(404).json(error.toString())
       throw error
     }
     response.status(200).json(results.rows)
@@ -18,7 +29,6 @@ const getTest = (request, response) => {
 
 const postTest = (request, response) => {
   const name = request.body.name
-
   pool.query('INSERT INTO test (name) values ($1)', [name], (error, results) => {
     if (error) {
       throw error
@@ -29,5 +39,6 @@ const postTest = (request, response) => {
 
 module.exports = {
 	getTest,
-    postTest
+    postTest,
+    psqlTest
 }
