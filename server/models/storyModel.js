7d82d04607683054cf431a0480de0b7f88f4f2dc
CecileJiaqiWang <jw81@rice.edu>
2019-10-04 22:56:01 -0500

    redefining story model

--- server/models/storyModel.js
+++ server/models/storyModel.js
@@ -1,53 +1,34 @@
 const pool = require('./db');
-// storyID (primary key) varchar
-// gridID (required) varchar
-// timestamp (required, in UTC) TIMESTAMP
-// text (optional) varchar
-// bucketName (optional) varchar
+// coordinate (required) [lat, lng]
+// timestamp (required, in UTC) string
+// text (optional) string
+// image(optional) image
 
 
 class Story {
     // Story object constructor
     constructor(story) {
-        this.storyID = story.storyID;
-        this.gridID = story.gridID;
+        this.coordinate = story.coordinate;
+        this.timestamp = story.timestamp;
         this.text = story.text;
-        this.ts = new Date().toUTCString();
-        this.bucketName = getBucketName(this.gridID);
+        this.image = story.image;
     }
 
     static createStory(story, result) {
         pool.query("INSERT INTO stories " +
-            "(storyID, gridID, ts, text, bucketName) values " +
-            "($1, $2, $3, $4, $5)",
-            [story.storyID, story.gridID, story.ts, story.text, story.bucketName],
+            "(lat, lng, timestamp, text) values " +
+            "($1, $2, $3, $4)",
+            [story.coordinate[0], story.coordinate[1], story.timestamp, story.text],
             function (err, record) {
                 if (err) {
                     console.log("error: ", err);
                     result(err, null);
                 } else {
-                    console.log(record.insertId);
                     result(null, record.insertId);
                 }
             });
+        // TODO: storing img in S3
     }
-
-    static getStory(result) {
-        pool.query("SELECT * FROM stories ORDER BY ts DESC LIMIT 10",
-            (err, record) => {
-                if (err) {
-                    console.log("error: ", err);
-                    result(err, null);
-                } else {
-                    result(null, record);
-                }
-            });
-    }
-}
-
-// Bucketname is hardcoded at this point.
-function getBucketName(gridID) {
-    return "1";
 }
 
 module.exports = Story;
