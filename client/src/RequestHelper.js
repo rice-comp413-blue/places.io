7079019ba2a43bd4d701325213b6876afc871579
klin1344 <kevlin97@gmail.com>
2019-10-07 15:39:54 -0500

    Client: Change from GET to POST

--- client/src/RequestHelper.js
+++ client/src/RequestHelper.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 const RequestHelper = {
     queryViewBoundingBox: (upperLeft, bottomRight) => {
-        return axios.get('', { latlng1: upperLeft, latlng2: bottomRight });
+        return axios.post('', { latlng1: upperLeft, latlng2: bottomRight });
     }
 }
 export default RequestHelper;
