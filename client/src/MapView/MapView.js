647874ac756fb0248a63e5a4a44a0f87c7dd4789
klin1344 <kevlin97@gmail.com>
2019-10-20 21:10:04 -0500

    Client: Optimize the number of View queries sent to GeoProxy #60

--- client/src/MapView/MapView.js
+++ client/src/MapView/MapView.js
@@ -2,44 +2,59 @@ import React from 'react';
 import '../App.css';
 import { Map, TileLayer } from 'react-leaflet';
 import RequestHelper from '../RequestHelper';
+import Control from 'react-leaflet-control';
+import Button from 'react-bootstrap/Button';
+
 class MapView extends React.Component {
     constructor(props) {
         super(props);
         this.map = undefined;
+        this.state = {
+            currentZoom: 10
+        };
     }
     componentDidMount() {
 
-        this.map.leafletElement.on('moveend', () => {
-            if (this.props.mode === 'view') {
-                // console.log(this.map)
-                const southWestBounds = this.map.leafletElement.getBounds().getSouthWest().wrap();
-                const northEastBounds = this.map.leafletElement.getBounds().getNorthEast().wrap();
-                const upperLeft = [northEastBounds.lat, southWestBounds.lng].map(x => Number(x));
-                const bottomRight = [southWestBounds.lat, northEastBounds.lng].map(x => Number(x));
-
-                // console.log(upperLeft, bottomRight);
-                //  query view
-                RequestHelper.queryViewBoundingBox(upperLeft, bottomRight)
-                    .then(res => {
-                        console.log(res);
-                        //  TODO: update shared state so that Sidebar can display results
-                        //  TODO: update markers for display on map
-                    })
-                    .catch(err => console.log(err));
-            }
+        this.map.leafletElement.on('zoomend', () => {
+            this.setState({ currentZoom: this.map.leafletElement.getZoom() });
         });
     }
     handleClick(e) {
         this.props.updateLatLngFunc({ lat: e.latlng.lat, lng: e.latlng.lng });
     }
+    handleViewClick() {
+        const southWestBounds = this.map.leafletElement.getBounds().getSouthWest().wrap();
+        const northEastBounds = this.map.leafletElement.getBounds().getNorthEast().wrap();
+        const upperLeft = [northEastBounds.lat, southWestBounds.lng].map(x => Number(x));
+        const bottomRight = [southWestBounds.lat, northEastBounds.lng].map(x => Number(x));
+
+        console.log(upperLeft, bottomRight);
+        //  query view
+        RequestHelper.queryViewBoundingBox(upperLeft, bottomRight)
+            .then(res => {
+                console.log(res);
+                //  TODO: update shared state so that Sidebar can display results
+                //  TODO: update markers for display on map
+            })
+            .catch(err => console.log(err));
+    }
     render() {
+
         return (
             <Map ref={(ref) => { this.map = ref; }} onClick={this.handleClick.bind(this)} center={[29.749907, -95.358421]} zoom={10}
-                style={{ height: '100vh', width: '100%' }}>
+                style={{ height: '90vh', width: '100%' }}>
                 <TileLayer
                     attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
+
+                {this.props.mode === 'view' && this.state.currentZoom > 6 ?
+                    <Control position="topright" >
+                        <Button onClick={() => this.handleViewClick.bind(this)}>
+                            Query
+                        </Button>
+                    </Control>
+                    : null}
             </Map>
         );
     }
