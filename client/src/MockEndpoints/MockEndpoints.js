71896b4e63b8359bbab411325589877a04f3dc36
Edward  Butler <edwardbutler@edwards-mbp.dyndns.rice.edu>
2019-10-28 15:44:02 -0500

    Update card syle

--- client/src/MockEndpoints/MockEndpoints.js
+++ client/src/MockEndpoints/MockEndpoints.js
@@ -4,12 +4,15 @@ const MockEndpoints = {
     view: (upperLeft, bottomRight) => {
         const results = [];
         const textInputs = ["Hey, I'm at Rice!", "What's up guys?", "Check out my story"];
+        const imageUrls = ["https://cdn.shopify.com/s/files/1/3004/1474/products/orange-tabby_1800x1800.png?v=1544042837","https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg?cs=srgb&dl=adorable-animal-breed-1108099.jpg&fm=jpg", "https://avatars1.githubusercontent.com/u/23466848?s=400&v=4"]
         for (let i = 0; i < 10; i++) {
             const text = textInputs[_.random(0, textInputs.length - 1)];
+            const imageURL = imageUrls[_.random(0, imageUrls.length - 1)]
             results.push({
                 storyid: i,
                 lat: _.random(upperLeft[0], bottomRight[0], true),
                 long: _.random(upperLeft[1], bottomRight[1], true),
+                imageURL, 
                 text,
                 timestamp: new Date()
             });
--- client/src/Sidebar/Feed/FeedItem.js
+++ client/src/Sidebar/Feed/FeedItem.js
@@ -1,14 +1,68 @@
 import React from 'react';
 
-const FeedItem = (props) => {
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+  card: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 140,
+  },
+});
+
+export default function MediaCard(props) {
+    const classes = useStyles();
+  
     return (
-        <div className="feedItem">
+      <Card className={classes.card}>
+        <CardActionArea>
+          <CardMedia
+            className={classes.media}
+            image={props.story.imageURL}
+            title="Story image"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              {props.story.text}
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
             <p>ID: {props.story.storyid}</p>
             <p>Text: {props.story.text}</p>
             <p>Location: ({props.story.lat}, {props.story.long})</p>
-            <p>Timestamp: {props.story.timestamp.toString()}</p>
-        </div>
+            {console.log(props.story.timestamp)}
+            <p>Timestamp: {props.story.timestamp.toLocaleString()}</p>
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          {/* <Button size="small" color="primary">
+            Share
+          </Button>
+          <Button size="small" color="primary">
+            Learn More
+          </Button> */}
+        </CardActions>
+      </Card>
     );
-};
+  }
 
-export default FeedItem
+// const FeedItem = (props) => {
+//     return (
+//         <div className="feedItem">
+//             <p>ID: {props.story.storyid}</p>
+//             <p>Text: {props.story.text}</p>
+//             <p>Location: ({props.story.lat}, {props.story.long})</p>
+//             <p>Timestamp: {props.story.timestamp.toString()}</p>
+//         </div>
+//     );
+// };
+
+// export default FeedItem
