2c073bcd87608d307989b325b32954b681273a55
Sung Soo (Anthony) Cho <sc80@rice.edu>
2019-11-04 05:31:34 -0600

    Added page limit parameter for box query

--- server/models/boxModel.js
+++ server/models/boxModel.js
@@ -9,13 +9,14 @@ class Box {
         this.latlng1 = box.latlng1;
         this.latlng2 = box.latlng2;
         this.skip = box.skip;
+        this.pagelimit = box.pagelimit;
     }
 
     static getStoriesInBox(box, result) {
         pool.query("SELECT * FROM story where " +
             "lat <= $1 and lat >= $2 and long <= $3 and long >= $4 " +
-            "ORDER BY timestamp DESC OFFSET $5 LIMIT 10",
-            [box.latlng1[0], box.latlng2[0], box.latlng2[1], box.latlng1[1], box.skip],
+            "ORDER BY timestamp DESC OFFSET $5 LIMIT $6",
+            [box.latlng1[0], box.latlng2[0], box.latlng2[1], box.latlng1[1], box.skip, box.pagelimit],
             (err, record) => {
                 if (err) {
                     console.log("error: ", err);
