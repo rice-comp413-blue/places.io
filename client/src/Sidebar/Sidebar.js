85b669b836c5a69e1bd25e7a2f75fed1595594f5
klin1344 <kevlin97@gmail.com>
2019-11-06 15:52:52 -0600

    Client: Start implementing react-paginate

--- client/src/Sidebar/Sidebar.js
+++ client/src/Sidebar/Sidebar.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import Feed from './Feed/Feed';
 import Form from './Feed/Form';
-import { SegmentedControl } from 'segmented-control'
+import { SegmentedControl } from 'segmented-control';
+import ReactPaginate from 'react-paginate';
 
-
+const handlePageClick = (data) => {
+    // TODO: Implement
+    console.log(data);
+}
 const Sidebar = (props) => {
     const updateMode = (newMode) => props.updateModeFunc(newMode);
     return (
@@ -17,7 +21,23 @@ const Sidebar = (props) => {
                 setValue={newMode => updateMode(newMode)}
                 style={{ width: '100%', color: '#2980b9' }} // purple400
             />
-            {props.mode === 'view' ? <Feed elements={props.feed} /> : <Form updateLatLngFunc={props.updateLatLngFunc.bind(this)} curLatLng={props.curLatLng}></Form>}
+            {props.mode === 'view' ?
+                <Feed elements={props.feed} />
+                : <Form updateLatLngFunc={props.updateLatLngFunc.bind(this)} curLatLng={props.curLatLng}></Form>}
+            {props.mode === 'view' && props.feed ?
+                <ReactPaginate
+                    previousLabel={'previous'}
+                    nextLabel={'next'}
+                    breakLabel={'...'}
+                    breakClassName={'break-me'}
+                    pageCount={10 /*TODO: Make query.*/}
+                    marginPagesDisplayed={2}
+                    pageRangeDisplayed={5}
+                    onPageChange={handlePageClick}
+                    containerClassName={'pagination'}
+                    subContainerClassName={'pages pagination'}
+                    activeClassName={'active'}
+                /> : undefined}
         </div>
     )
 
