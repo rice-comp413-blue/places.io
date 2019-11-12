59fd948a577cadb0318cb84f180a165c65eb31fa
EC2 Default User <ec2-user@ip-172-31-87-88.ec2.internal>
2019-11-12 01:48:23 +0000

    Removing changes to server code

--- server/server.js
+++ server/server.js
@@ -1,42 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const db = require('./queries');
 const story = require('./controllers/storyController');
 const validationMiddleware = require('./middleware/validation.request.property');
-const upload = require('./models/s3');
+const upload = require('./connection/s3');
 
 const app = express();
-//const port = 3000;
-const port = 1332;
+const port = 3000;
 
 app.use(cors());
-
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-);
+app.get('/', (request, response) => { response.json({ info: 'places.io server' }) });
 
-app.get('/', (request, response) => {
-  response.json({ info: 'Node.js, Express, and Postgres API' })
-});
+app.post('/view', validationMiddleware.validViewRequestProperties, story.getStoriesInBox); // view stories
 
-app.get('/test', db.getTest);
-app.post('/test', db.postTest);
+app.post('/submit', upload.single('file'), validationMiddleware.validSubmitRequestProperties, story.createStory);
 
 app.get('/health', story.healthStory); // health check
 
-// app.post('/submit', validationMiddleware.validSubmitRequestProperties, upload.single('file'), story.createStory);
-app.post('/submit', upload.single('file'), story.createStory); // create story
-
-app.post('/view', (req, res, next) => {
-	        console.log("View request received");
-	        console.log(req);
-	        next();
-}, validationMiddleware.validViewRequestProperties, story.getStoriesInBox); // view stories
+app.get('/count', story.getTotalStoryCount); // count stories
 
 app.listen(port, () => {
     console.log(`App running on port ${port}.`);
