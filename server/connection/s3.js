7d5309b7c407d91bfaca491ecbd7eb143b1352ff
yorhodes <yorkerhodesiv@gmail.com>
2019-11-15 16:01:29 -0600

    Use secrets.json

--- server/connection/s3.js
+++ server/connection/s3.js
@@ -1,22 +1,22 @@
-const auth = require('./connection').s3_auth;
-var multer  = require('multer');
-var multerS3 = require('multer-s3');
-const AWS = require('aws-sdk');
+var multer = require("multer");
+var multerS3 = require("multer-s3");
+const AWS = require("aws-sdk");
+const secrets = require("../../secrets.json");
 
-const s3 = new AWS.S3(auth);
-const BUCKET = 'comp413-places';
+const s3 = new AWS.S3(secrets.AWS.users.application);
+const BUCKET = "comp413-places";
 
 var upload = multer({
   storage: multerS3({
     s3: s3,
     bucket: BUCKET,
-    metadata: function (req, file, cb) {
-      cb(null, {originalName: file.originalname});
+    metadata: function(req, file, cb) {
+      cb(null, { originalName: file.originalname });
     },
-    key: function (req, file, cb) {
-      cb(null, Date.now() + file.originalname)
+    key: function(req, file, cb) {
+      cb(null, Date.now() + file.originalname);
     }
   })
-})
+});
 
-module.exports = upload
+module.exports = upload;
