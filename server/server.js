bf2045ef2b9c57cd8e10108094e7940c06e2bdd2
EC2 Default User <ec2-user@ip-172-31-87-88.ec2.internal>
2019-11-16 23:56:52 +0000

    Undoinng changes to server

--- server/server.js
+++ server/server.js
@@ -6,23 +6,7 @@ const validationMiddleware = require('./middleware/validation.request.property')
 const upload = require('./connection/s3');
 
 const app = express();
-const port = 1331;
-//const port = 3000;
-
-const mockResponse = {
-	  entries: [
-		      {
-			            "storyid": "def981c0-f059-11e9-b7b7-2917fbf11a40",
-			            "timestamp": "2019-10-17T02:12:18.000Z",
-			            "lat": 9,
-			            "long": 9,
-			            "text": "helloworld",
-			            "image_url": null
-			}
-	  ],
-	id:"ee66bf5b-524b-4786-ba88-0e9e8026dbca"
-}
-
+const port = 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -30,22 +14,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (request, response) => { response.json({ info: 'places.io server' }) });
 
-//app.post('/view', 
-//	(req,resp,next) => {
-//	console.log(req);
-//	next();
-//}, validationMiddleware.validViewRequestProperties, story.getStoriesInBox); // view stories
-
-app.post('/view',
-	        (req,resp,next) => {
-			console.log(req.body);
-			//console.log(req);
-			mockResponse.id=req.body.id;
-			resp.send(mockResponse); // Send hard-coded response
-			return ; // Stop early here. 
-		}, validationMiddleware.validViewRequestProperties, story.getStoriesInBox); // view stories
-
-// res.send({id:aaa})
+app.post('/view', validationMiddleware.validViewRequestProperties, story.getStoriesInBox); // view stories
 
 app.post('/submit', upload.single('file'), validationMiddleware.validSubmitRequestProperties, story.createStory);
 
