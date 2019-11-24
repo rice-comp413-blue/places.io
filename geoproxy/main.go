fd0eab251cf88a23ef9cc29cc10060d29923ec61
Ubuntu <ubuntu@ip-172-31-22-217.ec2.internal>
2019-11-24 20:06:16 +0000

    Removed a lot of the things we were using for the overly simplistic proxy

--- geoproxy/main.go
+++ geoproxy/main.go
@@ -10,7 +10,6 @@ package main
 import (
 	//"errors"
 	"bytes"
-	"encoding/binary"
 	"encoding/json"
 	"flag"
 	"fmt"
@@ -23,13 +22,11 @@ import (
 	"os"
 	"reflect"
 	"strconv"
-	"sync"
 	"time"
     aws "github.com/aws/aws-sdk-go/aws"
     session "github.com/aws/aws-sdk-go/aws/session"
     servicediscovery "github.com/aws/aws-sdk-go/service/servicediscovery"
 	"github.com/NYTimes/gziphandler"
-	uuid "github.com/google/uuid"
 )
 
 var verbose = false
@@ -95,9 +92,6 @@ type submitRequestPayloadStruct struct {
 	LatLng []float64 `json:"coordinate"`
 }
 
-type viewRequestHandler struct {
-}
-
 type singleViewRequestHandler struct {
 }
 
@@ -134,22 +128,6 @@ var timeout float64 = 1
 
 const entriesToServe = 10 // Currently hard-coded. do we want to take this in from the client or something?
 
-// Map of UUID to response writer
-var responseWriterMap map[uuid.UUID]http.ResponseWriter = make(map[uuid.UUID]http.ResponseWriter)
-
-// Map of UUID to mutex. Each mutex regulates access to the entries of the map corresponding to the respective UUID.
-var requestMutexMap map[uuid.UUID]sync.Mutex = make(map[uuid.UUID]sync.Mutex)
-
-// Mutex for regulating access to below maps. Must have this mutex before attempting to add or delete to the map
-var mapMutex = sync.Mutex{}
-
-// Map of UUID to number of requests expected
-var responsesMap map[uuid.UUID]int = make(map[uuid.UUID]int)
-
-// Following maps and mutex created to help handle forwarding of requests to multiple servers
-// Map of UUID to Heap for maintaining the entries we're sending back to client
-var queryMap map[uuid.UUID]PostList = make(map[uuid.UUID]PostList)
-
 // Get env var or default
 func getEnv(key, fallback string) string {
 	if value, ok := os.LookupEnv(key); ok {
@@ -172,42 +150,6 @@ func getNullResponseObj(id string) ResponseObj {
 	return respObj
 }
 
-// Log the env variables required for a reverse proxy
-func logSetup() {
-	a_condtion_url := os.Getenv("A_CONDITION_URL")
-	b_condtion_url := os.Getenv("B_CONDITION_URL")
-	default_condtion_url := os.Getenv("DEFAULT_CONDITION_URL")
-
-	log.Printf("Server will run on: %s\n", getListenAddress())
-	log.Printf("Redirecting to A url: %s\n", a_condtion_url)
-	log.Printf("Redirecting to B url: %s\n", b_condtion_url)
-	log.Printf("Redirecting to Default url: %s\n", default_condtion_url)
-}
-
-// Setups the mapping to servers
-func setupMap() {
-	// Array holds env strings
-	serverURLs = []string{"A_CONDITION_URL", "B_CONDITION_URL"}
-
-	// Map will map lat-long ranges to index in serverURLs
-	// latitude: (-90, 90) longitude: (-180, 180)
-
-	coordBoxToServer[cr1] = map[CoordRange]int{}
-	coordBoxToServer[cr2] = map[CoordRange]int{}
-	coordBoxToServer[cr1][cr3] = 0
-	coordBoxToServer[cr2][cr3] = 1
-}
-
-func modifyMap(conditional int) {
-	if conditional == 0 {
-		coordBoxToServer[cr1][cr3] = 1
-	} else if conditional == 1 {
-		coordBoxToServer[cr2][cr3] = 0
-	}
-
-}
-
-// Get a json decoder for a given requests body
 func requestBodyDecoder(request *http.Request) *json.Decoder {
 	// Read body to buffer
 	body, err := ioutil.ReadAll(request.Body)
@@ -435,83 +377,7 @@ func isJSON(s string) bool {
 	return json.Unmarshal([]byte(s), &js) == nil
 }
 
-// Takes in the new latlng box, response writer, request, unmarshalled view request payload, and uuid for request
-func serveViewReverseProxy(targets map[string]CoordBox, res http.ResponseWriter, req *http.Request, reqPayload viewRequestPayloadStruct, id uuid.UUID) {
-	if verbose {
-		log.Println("Making view request")
-	}
-	req.Header.Set("X-Forwarded-Host", req.Header.Get("Host"))
-
-	// Edit request body to include id
-	reqPayload.ID = id.String()
-
-	if verbose {	
-		fmt.Printf("%v \n", reqPayload)
-	}
-
-	setupTimer(id)
-	// Send to other servers for view request
-	for target, coordBox := range targets {
-		if verbose {
-			fmt.Printf("\tURL serving to: %s \n", target)
-		}
-		logViewRequestPayload(reqPayload, target)
-
-		// parse the url
-		url, _ := url.Parse(target)
-		url.Path = "/view"
-
-		// Edit coord box to intersecting box with server region
-		reqPayload.LatLng1 = []float64{coordBox.TopLeft.Lat, coordBox.TopLeft.Lng}
-		reqPayload.LatLng2 = []float64{coordBox.BottomRight.Lat, coordBox.BottomRight.Lng}
-		newReq, err := json.Marshal(reqPayload)
-		if err != nil {
-			fmt.Println("\tError marshalling request payload for view: ", err)
-		}
-		newReqBody := ioutil.NopCloser(bytes.NewBuffer(newReq))
-
-		if verbose {
-			log.Printf("\tServing request to %s: %s", target, string(newReq))
-		}
-
-		resp, err := http.Post(url.String(), "application/json", newReqBody)
-		if err != nil {
-			log.Printf("\tError when sending request: %v", err)
-			processResponse(getNullResponseObj(id.String()))
-		} else {
-			var responseObj ResponseObj
-			body, bodyErr := ioutil.ReadAll(resp.Body)
-			if bodyErr != nil {
-				// Do we want to stop the program here
-				log.Fatal("\tError reading view response: ", bodyErr)
-				http.Error(res, "Couldn't read server response body.", http.StatusInternalServerError)
-			}
-
-			if unmarshalErr := json.Unmarshal([]byte(body), &responseObj); unmarshalErr != nil {
-				log.Println("\tError unmarshalling view response: ", unmarshalErr)
-				http.Error(res, "Received invalid response from server", http.StatusInternalServerError)
-			} else {
-				// We got a valid response back and want to parse it out
-				processResponse(responseObj)
-			}
-			if verbose {
-				log.Printf("\tRequest served to reverse proxy for %s\n", target)
-				log.Printf("\tResponse obj: %v \n", responseObj)
-			}
-			resp.Body.Close() // Have to make sure to call this.
-		}
-	}
-}
-
-
 func buildProxy(proxy *httputil.ReverseProxy)  {
-
-	// proxy.ModifyResponse = func(r *http.Response) error {
-	// 		// return nil
-	// 		// purposefully return an error so ErrorHandler gets called
-	// 		return errors.New("uh-oh")
-	// }
-
 	proxy.ErrorHandler = func(rw http.ResponseWriter, r *http.Request, err error) {
 			fmt.Printf("error was: %+v \n", err)
 			rw.WriteHeader(http.StatusInternalServerError)
@@ -578,46 +444,13 @@ func enableCors(w *http.ResponseWriter) {
 	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 }
 
-// Sets up timeout callback. Call after sending all requests
-func setupTimer(id uuid.UUID) {
-	// How long we should wait for multiple
-	timeWait := 4 * time.Second
-	time.AfterFunc(timeWait, func() {
-		if verbose {
-			fmt.Printf("Timeout triggered for %s \n", id.String())
-		}
-		if mutex, ok := requestMutexMap[id]; ok {
-			mutex.Lock()
-			var readyToServe = false
-			if numRequests, exists := responsesMap[id]; exists {
-				if numRequests > 0 {
-					// Note, this should always be true if the request hasn't been serviced yet. maybe delete if statement
-					// Let's just send what we have
-					fmt.Printf("Got all responses back for request w/ ID: %s \n", id.String())
-					readyToServe = true
-				}
-			}
-			mutex.Unlock()
-			if readyToServe {
-				if verbose {
-					fmt.Println("serving response after cleanup")
-				}
-				// TODO: Currently getting superfluous response.WriteHeader err due to not counting empty/error responses
-				serveResponseThenCleanup(id)
-			}
-		}
-		/*
-			If the mutex isn't in the map, then this means that the request has already been served. In this case, we do nothing.
-		*/
-	})
-}
-
 func serveCountRequest(res http.ResponseWriter, req *http.Request) {
 	if req.Method == "OPTIONS" {
 		enableCors(&res)
 		res.WriteHeader(http.StatusOK)
 		return
 	}
+
 	var bodyBytes []byte
 	if req.Body != nil {
   		bodyBytes, _ = ioutil.ReadAll(req.Body)
@@ -711,59 +544,6 @@ func (rh *singleViewRequestHandler) ServeHTTP(res http.ResponseWriter, req *http
 	proxy.ServeHTTP(res, req)
 }
 
-// Given a request send it to the appropriate url
-func (rh *viewRequestHandler) ServeHTTP(res http.ResponseWriter, req *http.Request) {
-
-	if req.Method == "OPTIONS" {
-		enableCors(&res)
-		res.WriteHeader(http.StatusOK)
-		return
-	}
-		// View request
-		var tag = uuid.New()
-	
-		requestPayload := parseViewRequestBody(req)
-	
-	if reflect.DeepEqual(requestPayload, (viewRequestPayloadStruct{})) {
-		// If we get empty struct then something went wrong
-		http.Error(res, "Client sent incorrect view request body", http.StatusBadRequest)
-		return
-	}
-		urls := getBoundingBoxURLs(requestPayload.LatLng1, requestPayload.LatLng2)
-		if len(urls) == 0 {
-			http.Error(res, "Client's queried bounding box is invalid", http.StatusBadRequest)
-			return
-		}
-		
-		if verbose {
-			fmt.Printf("View request received\n")
-			fmt.Printf("Conditional url(s) attained\n")
-			fmt.Println(urls)
-		}
-		// Create an entry in our response map
-
-		var responseCount = 0
-
-		for url := range urls {
-			// todo: make sure that this is returning the actual url and not index
-			if url == ERROR_URL {
-				fmt.Println("Error: Could not send request due to incorrect request body")
-				http.Error(res, "Invalid bounding box queried", http.StatusBadRequest)
-				return
-			}
-			responseCount = responseCount + 1
-		}
-		// Make all map entries for this uuid
-		mapMutex.Lock()
-		responseWriterMap[tag] = res
-		queryMap[tag] = make(PostList, 0)
-		responsesMap[tag] = responseCount
-		requestMutexMap[tag] = sync.Mutex{}
-		mapMutex.Unlock()
-
-		serveViewReverseProxy(urls, res, req, requestPayload, tag)
-}
-
 func checkMatches(resp *http.Response) bool {
 	var p []byte
 
@@ -795,188 +575,6 @@ func checkMatches(resp *http.Response) bool {
 	return (false)
 }
 
-// This function is called on a timer and pings both
-// servers with a GET request. A 302 response is expected
-// but not yet explicity checked
-func checkHealth(ticker *time.Ticker, done chan bool) {
-	//Reference for ticker code:
-	for {
-		select {
-		//Is there any case we want this ticker to stop? Leaving for now in case
-		// we want to modify this
-		case <-done:
-			return
-		//case t := <-ticker.C:
-		case <-ticker.C:
-			client := &http.Client{
-				//Reference here for redirect code: https://jonathanmh.com/tracing-preventing-http-redirects-golang/
-				CheckRedirect: func(req *http.Request, via []*http.Request) error {
-					return http.ErrUseLastResponse
-				}}
-
-			if pingA {
-				resp, err := client.Get(os.Getenv("A_CONDITION_URL") + "/health")
-
-				// Modified for testing
-				//What should we do if we run into an error? Right now just printing it
-				if err != nil {
-					fmt.Println(err)
-					modifyMap(0)
-					pingA = false
-				} else {
-					valid := checkMatches(resp)
-
-					if valid != true {
-						fmt.Println("Error in response JSON during health check")
-						modifyMap(0)
-						pingA = false
-					}
-				}
-			}
-
-			if pingB {
-				resp, err2 := client.Get(os.Getenv("B_CONDITION_URL") + "/health")
-
-				// Modified for testing
-				if err2 != nil {
-					fmt.Println(err2)
-					modifyMap(1)
-					pingB = false
-				} else {
-					valid := checkMatches(resp)
-					if valid != true {
-						fmt.Println("Error in response JSON during health check")
-						modifyMap(1)
-						pingB = false
-					}
-				}
-			}
-
-		}
-	}
-}
-
-func processResponse(response ResponseObj) {
-	//b := []byte(response.ID)
-	var id, err = uuid.Parse(response.ID)
-	if verbose {
-		fmt.Printf("Processing response obj: %v \n", response)
-	}
-	if err != nil {
-		fmt.Println("Error parsing the response's id field into UUID")
-	} else {
-		// Concurrent reads of the map for getting the mutex are alright
-		if mutex, ok := requestMutexMap[id]; ok {
-			// make sure that this id hasn't been cleaned up
-			var readyToServe = false // Tells us whether all responses have been received
-			mutex.Lock()
-			var responseEntries = response.Posts
-			responsesMap[id] = responsesMap[id] - 1 // decrement number of responses we're waiting on
-			var pl = queryMap[id]
-			fmt.Printf("Len response %d \n", len(responseEntries))
-			for _, responseEntry := range responseEntries {
-				pl.PushToCapacity(entriesToServe, &responseEntry)
-			}
-			if responsesMap[id] == 0 {
-				if verbose {
-					log.Println("Got back all responses in time")
-				}
-				readyToServe = true
-			}
-			queryMap[id]=pl
-			mutex.Unlock()
-			if readyToServe {
-				if verbose {
-					log.Printf("About to serve request for id: %s\n", id)
-				}
-				
-				serveResponseThenCleanup(id)
-			}
-		} else {
-			if verbose {
-				log.Printf("Response for tag %s came after cleanup.", id)
-			}
-		}
-	}
-}
-
-func serveResponseThenCleanup(id uuid.UUID) {
-	if requestMutex, ok := requestMutexMap[id]; ok {
-		requestMutex.Lock() // We lock here in case we have two or more responses arrive after timeout
-		// We don't want both responses triggering us to serve the response.
-		// This shouldn't be a problem if all responses arrive in a timely manner though
-		defer requestMutex.Unlock()
-
-		// Get the response writer
-		var resWriter = responseWriterMap[id]
-		if pl, ok := queryMap[id]; ok {
-		// Get the actual structs corresponding to the pointers in the list
-		responseEntries := make([]Post, len(pl))
-		for i, post := range pl {
-			responseEntries[i]=*post
-		}
-
-		viewResponse := ViewResponseObj{responseEntries}
-
-		// First marshal the response
-		var data, marshErr = json.Marshal(viewResponse)
-		if marshErr != nil {
-			// TODO: check if we want to return this response code
-			http.Error(resWriter, "Received bad response for view request from server", http.StatusBadGateway)
-		}
-
-		data_b := []byte(data)
-		
-		if verbose {
-			fmt.Printf("Serving response body: %s \n", string(data_b))
-		}
-
-		// Set the appropriate things on the response writer
-		resWriter.Header().Set("Content-Type", "application/json")
-		//resWriter.Header().Set("Content-Length", string(1000))
-		resWriter.Header().Set("Content-Length", strconv.Itoa(len(data_b)))
-		//resWriter.WriteHeader(http.StatusOK)
-		// todo: check which status code we want
-		if verbose {
-			fmt.Printf("Num bytes: %d \n", binary.Size(data_b))
-		}
-
-		// serve the request to the client
-		i, writeErr := resWriter.Write(data_b)
-		if verbose {
-			log.Printf("Wrote: %d bytes to the client \n", i)
-		}
-		if writeErr != nil {
-			log.Println("Error writing to response writer: ", writeErr)
-		}
-
-		// Clean up
-		multiServerMapCleanup(id)
-		} else {
-			// If the id isn't present then we somehow deleted it from the map before servicing the request. 
-			if verbose { 
-				fmt.Printf("response for request with id %s was removed from the map before servicing the request \n", id.String())
-			}
-			http.Error(resWriter, "Response was deleted from proxy memory before being served", http.StatusBadGateway)
-			return
-		}
-	}
-}
-
-func multiServerMapCleanup(id uuid.UUID) {
-	// First lock on mutex
-	mapMutex.Lock()
-	delete(requestMutexMap, id)
-	delete(responsesMap, id)
-	delete(queryMap, id)
-	delete(responseWriterMap, id)
-	if verbose {
-		log.Printf("Cleaned up maps for id: %s \n", id.String())
-		//log.Println("New map", queryMap)
-	}
-	mapMutex.Unlock()
-}
-
 func check_service() {
 	fmt.Println("Check service.")
 	sess := session.Must(session.NewSessionWithOptions(session.Options{
@@ -1002,8 +600,6 @@ func main() {
 	check_service()
 
 	// Log setup values
-	logSetup()
-	setupMap()
 	flag.BoolVar(&verbose, "v", false, "a bool")
 	flag.Parse()
 	if verbose {
@@ -1025,11 +621,6 @@ func main() {
 	//http.HandleFunc("/", handleRequestAndRedirect)
 	//http.HandleFunc("/", testFixedResponse)
 	//Initialize ticker + channel + run in parallel
-	/*
-	ticker := time.NewTicker(5000 * time.Millisecond)
-	done := make(chan bool)
-	go checkHealth(ticker, done)
-	*/
 	if err := http.ListenAndServe(getListenAddress(), nil); err != nil {
 		fmt.Println("Error when calling listen and serve")
 		panic(err)
