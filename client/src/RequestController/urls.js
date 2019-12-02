a8f663d8f2cfdf0e4ec582b2b5aa52062f9b99bc
klin1344 <kevlin97@gmail.com>
2019-12-02 09:40:52 -0800

    Client: Update URLS, update Leaflet consumables, small change in checking when whether we display image in FeedItem or not

--- client/src/RequestController/urls.js
+++ client/src/RequestController/urls.js
@@ -1,8 +1,8 @@
 const URLS = {
     //  URLS go here
-    submit: 'http://18.220.67.124:1330/submit',
-    view: 'http://18.220.67.124:1330/view',
-    count: 'http://18.220.67.124:1330/count',
+    submit: 'http://18.222.226.180:1330/submit',
+    view: 'http://18.222.226.180:1330/view',
+    count: 'http://18.222.226.180:1330/count',
     type: 'real'        //  'real' to use actual enpoints
 }
 export default URLS;
