d042d0f4b6e2be84d533e5a18a768cff405ddef3
Sung Soo (Anthony) Cho <sc80@rice.edu>
2019-12-03 17:44:29 -0600

    Added logo in navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import logo from './logo.svg';
+import logo from './Assets/logo.svg';
 import Navbar from 'react-bootstrap/Navbar';
 import './App.css';
 import MainApp from './MainApp/MainApp';
@@ -7,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <Navbar bg="light">
+        <img style={{ height: '100%', width: '2em', marginRight: '0.5em' }} alt="logo" src={logo} />
         <Navbar.Brand>places.io</Navbar.Brand>
       </Navbar>
       <MainApp />
